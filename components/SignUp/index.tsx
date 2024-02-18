57ee8f381a0d4dd8f88ecff7172332ecbef1981f
ZowWeb <zohaibandkhan@gmail.com>
2024-02-18 17:20:45 +0530

    Redirect user to welcome after successful sign up

--- components/SignUp/index.tsx
+++ components/SignUp/index.tsx
@@ -1,58 +1,70 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Button, Checkbox, Form, Input } from 'antd'
+import { useRouter } from 'next/navigation'
 
-const onFinish = (values: any) => {
-  console.log('Success:', values)
-}
-
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo)
-}
-
-type FieldType = {
+export type FieldType = {
   name: string
   email: string
   password: string
 }
 
-const SignUp = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Name"
-      name="name"
-      rules={[{ required: true, message: 'Please input your name!' }]}
+const SignUp = () => {
+  const router = useRouter()
+  const onFinish = useCallback(async (values: any) => {
+    console.log('Success:', values)
+    // make API request
+    try {
+      await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      })
+      router.push('/welcome')
+    } catch (e) {
+      console.error(e)
+    }
+  }, [])
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      autoComplete="off"
     >
-      <Input />
-    </Form.Item>
-    <Form.Item<FieldType>
-      label="Email"
-      name="email"
-      rules={[{ required: true, message: 'Please input your email!' }]}
-    >
-      <Input type="email" />
-    </Form.Item>
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-)
+      <Form.Item<FieldType>
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: 'Please input your name!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item<FieldType>
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: 'Please input your email!' }]}
+      >
+        <Input type="email" />
+      </Form.Item>
+      <Form.Item<FieldType>
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
 
 export default SignUp
